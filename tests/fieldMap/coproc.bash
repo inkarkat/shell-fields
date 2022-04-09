42bdaab25358e1f2150644d8af4b4223ee498b1e
Ingo Karkat <swdev@ingo-karkat.de>
2022-04-09 14:19:50 +0200

    Tests: Add coverage for failing coproc command

--- tests/fieldMap/coproc.bash
+++ tests/fieldMap/coproc.bash
@@ -2,3 +2,25 @@
 
 readonly uppercaseCommand='sed --unbuffered -e y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/'
 readonly countCommand='sed --unbuffered -n ='
+
+runStdout() {
+  local origFlags="$-"
+  set +eET
+  local origIFS="$IFS"
+  output="$("$@" 2>/dev/null)"
+  status="$?"
+  IFS=$'\n' lines=($output)
+  IFS="$origIFS"
+  set "-$origFlags"
+}
+
+runStderr() {
+  local origFlags="$-"
+  set +eET
+  local origIFS="$IFS"
+  output="$("$@" 2>&1 >/dev/null)"
+  status="$?"
+  IFS=$'\n' lines=($output)
+  IFS="$origIFS"
+  set "-$origFlags"
+}
